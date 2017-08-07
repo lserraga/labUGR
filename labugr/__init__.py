a49aebd4d757be808bd46ce9fd64d0ec6b937910
Luis Serra Garcia <luikserra@gmail.com>
2017-08-07 14:01:05 -0400

    Definiendo funciones de Numpy
    Anadidas funciones para la creacion y modificacion de arrays

--- labugr/__init__.py
+++ labugr/__init__.py
@@ -1,6 +1,22 @@
-#__all__=['waveforms']
-
 from .scipy.waveforms import *
 from .scipy.respuestaF import *
+
+import numpy as np
+import matplotlib as plt
+
 from numpy import arange, pi
+
+#Creacion de arrays
+from numpy import (
+	array, zeros, ones, eye, full, asarray, copy, fromfunction,
+	arrange, linspace, logspace, geomspace, tri)
+
+#Guardando y cargando arrays
+from numpy import (
+	save, load, fromfile)
+
+#Propiedades del array
+from numpy import (
+	shape, ndim, size, dtype, real, imag, diag, tril ,triu)
+
 __all__ = [s for s in dir() if not (s=='waveforms' or s=='dependencias' or s.startswith('_'))]
