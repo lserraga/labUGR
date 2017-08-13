fe5d691bec00337e3fed61948d9e54a79699ed31
Luis Serra Garcia <luikserra@gmail.com>
2017-08-13 12:38:02 -0400

    Script para eliminar archivos de instalación (en developer0

---
+++ scripts/remove_build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Eliminar los archivos creados con pip3 install -e
+
+sudo rm -r labugr.egg-info
+
+sudo rm -r build
+
+sudo rm labugr/__config__.py 
+
+sudo rm labugr/fftpack/src/dct.c 
+
+sudo rm labugr/fftpack/src/dst.c 
+
+sudo rm labugr/fftpack/convolvemodule.c 
+
+sudo rm labugr/fftpack/_fftpackmodule.c
+
+directorio=$(pwd)
+
+cd -P /usr/local/lib/python3.5/dist-packages
+
+sudo sed -i '/proyecto/d' ./easy-install.pth
+
+sudo rm labugr.egg-link
+
+cd -P $directorio
+
+echo Archivos de instalacion eliminados
