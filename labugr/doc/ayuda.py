628fb4e0e892f53bc097f2f93ee4fa783e159fcf
Luis Serra Garcia <luikserra@gmail.com>
2017-08-13 11:31:22 -0400

    Actualizado .gitignore para el modo developer

--- labugr/doc/ayuda.py
+++ labugr/doc/ayuda.py
@@ -1,7 +1,12 @@
 import os
 import difflib
 
+__all__ = ['ayuda']
+
 def __nombre_funciones():
+	"""
+	Devuelve una lista de las funciones en el directorio de trabajo
+	"""
 	lista = os.listdir()
 
 	for i in range(0, len(lista)):
@@ -11,13 +16,30 @@ def __nombre_funciones():
 
 
 def ayuda(funcion):
+	"""
+	Muestra en pantalla la documentación en español de una función.
+
+	Carga la documentación desde el directorio doc/funciones.
+
+	Parámetros
+    ----------
+    funcion : string o función
+    	El nombre de la función como string o como objeto en si 
+
+    Ejemplos
+    --------
+    >>> ayuda(arange)
+    >>> ayuda('arange')
+	"""
 	directorio_previo = os.getcwd()
 	os.chdir(os.path.join(os.path.dirname(__file__),'funciones'))
 
 	lista = __nombre_funciones()
 	
+	#Si la función es pasada como objeto, obtener su nombre como str
 	if not isinstance(funcion,str):
 		funcion = funcion.__name__
+	
 	if not funcion in lista:
 		parecida = difflib.get_close_matches(funcion, lista)
 		print("""
