af608ef86c6d0a5ad0f91a6e9df9663d4ee28aee
Luis Serra Garcia <luikserra@gmail.com>
2017-08-14 06:28:48 -0400

    Añadidas funciones para el anális espectral
    También lleva incluidos los tests

--- labugr/__init__.py
+++ labugr/__init__.py
@@ -27,6 +27,7 @@ if not __LABUGR_SETUP__:
 from .scipy.waveforms import *
 from .scipy.respuestaF import *
 from .scipy.windows import *
+from .scipy.spectral import *
 from .doc.ayuda import ayuda
 
 import numpy as np
